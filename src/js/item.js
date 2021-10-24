f1dd9e1a362c0f77d35cd38c9a75f3bad0e37ca5
DrMadhushan <dev.drmad@gmail.com>
2021-10-24 13:50:32 +0530

    have to make datasheet overlays

---
+++ src/js/item.js
@@ -0,0 +1,6 @@
+const { webContents, contentTracing } = require("electron");
+
+function showDatasheet(){
+    let item_data = document.getElementById('item-details');
+    item_data.style.display = none;
+}
