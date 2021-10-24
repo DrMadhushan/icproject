9cceaf15475cff918309138cb2142a8d3c1a46fc
DrMadhushan <dev.drmad@gmail.com>
2021-10-24 13:51:09 +0530

    login from form data using api implemented

--- src/js/login.js
+++ src/js/login.js
@@ -28,6 +28,7 @@ function userLogin() {
     var info = document.getElementById('info-box');
     console.log("clicked");
     // e.preventDefault();
+    
     if (!(navigator.onLine)) {
         console.log("Offline")
         return;
@@ -35,7 +36,7 @@ function userLogin() {
 
     var email = document.getElementById('email');
     var pwd = document.getElementById('passcode');
-/*
+
     if (!validateEmail(email)){
         info.innerHTML = "The email address provided is invalid<br>Please re-enter your credentials!"
         return;
@@ -44,27 +45,34 @@ function userLogin() {
         info.innerHTML = "Invalid password!";
         return;
     }
-*/
+
     // authenticate using api
-    var apiLoginRoute = "https://" + "www.makerspace.com/api/user/login";
-    var dummy = 'https://jsonplaceholder.typicode.com/todos/1';
+    let api_host = 'http://127.0.0.1:8000/api'
+    // var apiLoginRoute = "https://" + "www.makerspace.com/api/user/login";
+    // var dummy = 'https://jsonplaceholder.typicode.com/todos/1';
 
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', dummy);
-    xhr.send();
+    let url = api_host + '/auth/login';
+    let data = `email=${email.value}&password=${pwd.value}`;
+    console.log(data);
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send(data);
 
-    console.log("before");
+    // console.log("before");
     xhr.onload = () => {
         dict = JSON.parse(xhr.response);
         console.log(dict);
         info.innerHTML = "Fetched!<br>" + xhr.response;
-        sessionStorage.setItem('current_user', dict.id);
-        sessionStorage.setItem('token', dict.title);
+        sessionStorage.setItem('user_token', dict.token);
         if (xhr.status == 200){
+            console.log("Status = 200 OK");
+            sessionStorage.setItem('user_token', dict.token);
             window.location.href = 'dashboard.html';
         }
         
     }
 
-    console.log("after");
+    // console.log("after");
 }
